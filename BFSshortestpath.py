9b9f6d29943aae6fdd1de7f000a49de0cd355704
unavaliablefish <165564774+unavaliablefish@users.noreply.github.com>
2025-03-21 10:06:09 +0800

    Update BFSshortestpath.py

--- BFSshortestpath.py
+++ BFSshortestpath.py
@@ -4,22 +4,18 @@ def bfs_shortest_path(n, m, edges):
     graph = [[] for _ in range(n + 1)]
     for x, y in edges:
         graph[x].append(y)
-
     queue = deque([1])
     distance = [-1] * (n + 1)
     distance[1] = 0
-
     while queue:
         u = queue.popleft()
         for v in graph[u]:
             if distance[v] == -1:
                 queue.append(v)
                 distance[v] = distance[u] + 1
-
     return distance[n]
 
 n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
-
 print(bfs_shortest_path(n, m, edges))
 
